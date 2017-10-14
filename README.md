8c3fdc44a621a210e4f8d81411d1fdaf5776e5ec
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-10-14 12:51:33 -0300

    adicionando arquivos do projeto

---
+++ README.md
@@ -0,0 +1,27 @@
+# List Events
+
+desenvolvimento de um web app de exibição de eventos
+
+## Stack Project
+- HTML, CSS(SASS)
+
+## Folders Structure
+    .
+    |
+    |
+    |
+
+## Run the project locally
+**1 -** Clone the project and install the dependencies:
+```sh
+$ git clone https://github.com/lucasaugustofrontend/list-events.git
+$ cd list-events
+$ npm i
+```
+**2 -** Run static server and livereload
+```sh
+$ npm run gulp
+```
+
+## Automatic Tasks
+- `$ npm run gulp`: Watch the files and build and start a static server.
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp')
+const connect = require('gulp-connect')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('default', ['server', 'watch'])
+
+gulp.task('copy', () => {
+  gulp.src(['src/*.html', 'src/assets/css/**/*', 'src/assets/images/**/*'])
+    .pipe(gulp.dest('docs'))
+})
+
+gulp.task('server', () => {
+  connect.server({
+    root: 'src',
+    livereload: true,
+    port: 8080
+  })
+})
+
+gulp.task('sass', () => {
+  gulp.src('src/assets/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('src/assets/css'))
+})
+
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+    .pipe(connect.reload())
+})
+gulp.task('css', () => {
+  gulp.src('src/assets/css/**/*.css')
+    .pipe(connect.reload())
+})
+
+gulp.task('watch', () => {
+  gulp.watch('src/*.html', ['html'])
+  gulp.watch('src/assets/sass/**/*.scss', ['sass'])
+  gulp.watch('src/assets/css/**/*.css', ['css'])
+})
